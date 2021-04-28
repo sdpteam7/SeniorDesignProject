21bc1336c82e1e56df84bb9d4db4607cd69a8395
sdpteam7 <79306376+sdpteam7@users.noreply.github.com>
2021-04-28 11:05:12 +0400

    Delete src/main directory

--- src/main/java/com/example/finalproject/java/types/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalproject.java.types;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Constants extends AppCompatActivity {
-    public static String baseUrl = "https://aze-edu-v1.herokuapp.com/api/";
-}
